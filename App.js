322df18b79be0c0f5957cb5fbb27e56efe5a0b11
Yoan <yoannier@Yoans-MacBook-Pro.local>
2019-11-29 20:10:57 -0500

    Tik-Tak Mobile

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function App() {
+  return (
+    <WebView source={{ uri: 'https://tiktakapp.herokuapp.com' }} style={{ marginTop: 20 }} />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
